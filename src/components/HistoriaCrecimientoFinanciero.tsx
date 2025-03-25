fffd024c0fa1d454a57fe3cdf8eccef0b49a04ec
mendozalz <mendoza124302@gmail.com>
2025-03-25 17:01:52 -0500

    corregido las traducciones para la pagina "Historia de exitos"

--- src/components/HistoriaCrecimientoFinanciero.tsx
+++ src/components/HistoriaCrecimientoFinanciero.tsx
@@ -5,12 +5,95 @@ import {
   TabsTrigger,
 } from "./ui/tabsCrecimientoFinanciero";
 
-function HistoriaCrecimientoFinanciero() {
+/**
+ * Interfaz para las propiedades del componente HistoriaCrecimientoFinanciero
+ * @interface Props
+ * @property {Object} i18n - Objeto con las traducciones
+ */
+interface Props {
+  i18n: {
+    SUCCESS_STORIES: {
+      TITLE: string;
+      DESCRIPTION: string;
+      LIST: {
+        TITLE1: string;
+        TITLE2: string;
+        TITLE3: string;
+      };
+      TESTIMONIAL: {
+        TITLE: string;
+      };
+      CASES: {
+        CASE1: {
+          TITLE: string;
+          LIST1: {
+            TITLE: string;
+            DESCRIPTION: string;
+          };
+          LIST2: {
+            TITLE: string;
+            DESCRIPTION: string;
+          };
+          LIST3: {
+            TITLE: string;
+            DESCRIPTION: string;
+          };
+          TESTIMONIAL: {
+            QUOTE: string;
+          };
+        };
+        CASE2: {
+          TITLE: string;
+          LIST1: {
+            TITLE: string;
+            DESCRIPTION: string;
+          };
+          LIST2: {
+            TITLE: string;
+            DESCRIPTION: string;
+          };
+          LIST3: {
+            TITLE: string;
+            DESCRIPTION: string;
+          };
+          TESTIMONIAL: {
+            QUOTE: string;
+          };
+        };
+        CASE3: {
+          TITLE: string;
+          LIST1: {
+            TITLE: string;
+            DESCRIPTION: string;
+          };
+          LIST2: {
+            TITLE: string;
+            DESCRIPTION: string;
+          };
+          LIST3: {
+            TITLE: string;
+            DESCRIPTION: string;
+          };
+          TESTIMONIAL: {
+            QUOTE: string;
+          };
+        };
+      };
+    };
+  };
+}
+
+/**
+ * Componente que muestra historias de crecimiento financiero en un formato de pestañas
+ * @param {Props} props - Propiedades del componente
+ * @returns {JSX.Element} Componente de historias de crecimiento financiero
+ */
+function HistoriaCrecimientoFinanciero({ i18n }: Props) {
   return (
     <div>
       <div className="max-w-5xl mx-auto text-center mb-lg">
         <h2 className="text-2xl md:text-3xl font-bold mb-xs leading-none">
-          Historias de Crecimiento Financiero.
+          {i18n.SUCCESS_STORIES.TITLE}
         </h2>
       </div>
       <Tabs
@@ -23,98 +106,126 @@ function HistoriaCrecimientoFinanciero() {
             value="tab-1"
             className="w-full justify-start data-[state=active]:bg-muted data-[state=active]:shadow-none text-base"
           >
-            * Expansión de Patrimonio para un Empresario de Alto Nivel
+            * {i18n.SUCCESS_STORIES.CASES.CASE1.TITLE}
           </TabsTrigger>
           <TabsTrigger
             value="tab-2"
             className="w-full justify-start data-[state=active]:bg-muted data-[state=active]:shadow-none text-base"
           >
-            * Profesional que Buscaba Seguridad Financiera
+            * {i18n.SUCCESS_STORIES.CASES.CASE2.TITLE}
           </TabsTrigger>
           <TabsTrigger
             value="tab-3"
             className="w-full justify-start data-[state=active]:bg-muted data-[state=active]:shadow-none text-base"
           >
-            * Pequeño Inversionista Construyendo su Futuro
+            * {i18n.SUCCESS_STORIES.CASES.CASE3.TITLE}
           </TabsTrigger>
         </TabsList>
         <div className="grow rounded-lg border border-border text-start">
           <TabsContent value="tab-1">
             <p className="px-4 py-1.5 text-xs text-muted-foreground">
-              <b>- Perfil:</b> Inversionista con alta capacidad de capital,
-              empresario del sector tecnológico.
+              <b>- {i18n.SUCCESS_STORIES.CASES.CASE1.LIST1.TITLE}</b>{" "}
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: i18n.SUCCESS_STORIES.CASES.CASE1.LIST1.DESCRIPTION,
+                }}
+              />
             </p>
             <p className="px-4 py-1.5 text-xs text-muted-foreground">
-              <b>- Inversión:</b> Capital inicial de <b>$500,000 USD</b> en
-              <b> mercados financieros y bienes raíces comerciales.</b>
+              <b>- {i18n.SUCCESS_STORIES.CASES.CASE1.LIST2.TITLE}</b>{" "}
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: i18n.SUCCESS_STORIES.CASES.CASE1.LIST2.DESCRIPTION,
+                }}
+              />
             </p>
             <p className="px-4 py-1.5 text-xs text-muted-foreground">
-              <b>- Resultados:</b> En <b>3 años,</b> logró un{" "}
-              <b>retorno del 150%,</b> diversificando su portafolio y asegurando
-              ingresos pasivos en bienes raíces.
+              <b>- {i18n.SUCCESS_STORIES.CASES.CASE1.LIST3.TITLE}</b>{" "}
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: i18n.SUCCESS_STORIES.CASES.CASE1.LIST3.DESCRIPTION,
+                }}
+              />
             </p>
             <div className="flex items-end bg-gradient-to-t from-black/70 to-transparent">
               <div className="p-md md:p-lg text-gray-800">
                 <span className="badge-outline mb-xs border-white/20 bg-white/10 text-gray-800">
-                  <b>- Testimonio:</b>
+                  <b>- {i18n.SUCCESS_STORIES.TESTIMONIAL.TITLE}</b>
                 </span>
                 <p className="text-fl-lg font-medium">
-                  "Mi experiencia con Nexus Private Capital ha sido excepcional.
-                  Su enfoque estratégico y la transparencia en cada operación me
-                  han permitido expandir mi patrimonio sin asumir riesgos
-                  innecesarios."
+                  {i18n.SUCCESS_STORIES.CASES.CASE1.TESTIMONIAL.QUOTE}
                 </p>
               </div>
             </div>
           </TabsContent>
           <TabsContent value="tab-2">
             <p className="px-4 py-1.5 text-xs text-muted-foreground">
-              <b>- Perfil:</b> Médico con interés en inversiones a largo plazo.
+              <b>- {i18n.SUCCESS_STORIES.CASES.CASE2.LIST1.TITLE}</b>{" "}
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: i18n.SUCCESS_STORIES.CASES.CASE2.LIST1.DESCRIPTION,
+                }}
+              />
             </p>
             <p className="px-4 py-1.5 text-xs text-muted-foreground">
-              <b>- Inversión: $80,000 USD</b> en{" "}
-              <b>bienes raíces y activos de alta capitalización.</b>
+              <b>- {i18n.SUCCESS_STORIES.CASES.CASE2.LIST2.TITLE}</b>{" "}
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: i18n.SUCCESS_STORIES.CASES.CASE2.LIST2.DESCRIPTION,
+                }}
+              />
             </p>
             <p className="px-4 py-1.5 text-xs text-muted-foreground">
-              <b>- Resultados: Valorización del 35% en 2 años,</b> con ingresos
-              pasivos por alquiler de propiedades adquiridas.
+              <b>- {i18n.SUCCESS_STORIES.CASES.CASE2.LIST3.TITLE}</b>{" "}
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: i18n.SUCCESS_STORIES.CASES.CASE2.LIST3.DESCRIPTION,
+                }}
+              />
             </p>
             <div className="flex items-end bg-gradient-to-t from-black/70 to-transparent">
               <div className="p-md md:p-lg text-gray-800">
                 <span className="badge-outline mb-xs border-white/20 bg-white/10 text-gray-800">
-                  <b>- Testimonio:</b>
+                  <b>- {i18n.SUCCESS_STORIES.TESTIMONIAL.TITLE}</b>
                 </span>
                 <p className="text-fl-lg font-medium">
-                  "Siempre pensé que las inversiones eran complejas y
-                  arriesgadas, pero Nexus me mostró que con una estrategia
-                  adecuada, podía generar ingresos adicionales y asegurar mi
-                  futuro financiero."
+                  {i18n.SUCCESS_STORIES.CASES.CASE2.TESTIMONIAL.QUOTE}
                 </p>
               </div>
             </div>
           </TabsContent>
           <TabsContent value="tab-3">
             <p className="px-4 py-1.5 text-xs text-muted-foreground">
-              <b>- Perfil:</b> Empleado con ahorros personales buscando
-              crecimiento progresivo.
+              <b>- {i18n.SUCCESS_STORIES.CASES.CASE3.LIST1.TITLE}</b>{" "}
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: i18n.SUCCESS_STORIES.CASES.CASE3.LIST1.DESCRIPTION,
+                }}
+              />
             </p>
             <p className="px-4 py-1.5 text-xs text-muted-foreground">
-              <b>- Inversión: $10,000 USD</b> en{" "}
-              <b>mercados financieros con estrategia de bajo riesgo.</b>
+              <b>- {i18n.SUCCESS_STORIES.CASES.CASE3.LIST2.TITLE}</b>{" "}
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: i18n.SUCCESS_STORIES.CASES.CASE3.LIST2.DESCRIPTION,
+                }}
+              />
             </p>
             <p className="px-4 py-1.5 text-xs text-muted-foreground">
-              <b>- Resultados:</b> Crecimiento del <b>60% en 2 años,</b>{" "}
-              permitiéndole reinvertir y escalar su portafolio.
+              <b>- {i18n.SUCCESS_STORIES.CASES.CASE3.LIST3.TITLE}</b>{" "}
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: i18n.SUCCESS_STORIES.CASES.CASE3.LIST3.DESCRIPTION,
+                }}
+              />
             </p>
             <div className="flex items-end bg-gradient-to-t from-black/70 to-transparent">
               <div className="p-md md:p-lg text-gray-800">
                 <span className="badge-outline mb-xs border-white/20 bg-white/10 text-gray-800">
-                  <b>- Testimonio:</b>
+                  <b>- {i18n.SUCCESS_STORIES.TESTIMONIAL.TITLE}</b>
                 </span>
                 <p className="text-fl-lg font-medium">
-                  "Gracias a Nexus, aprendí que no se necesita ser millonario
-                  para invertir. Con una estrategia clara y acompañamiento
-                  experto, pude hacer crecer mis ahorros de forma segura."
+                  {i18n.SUCCESS_STORIES.CASES.CASE3.TESTIMONIAL.QUOTE}
                 </p>
               </div>
             </div>
